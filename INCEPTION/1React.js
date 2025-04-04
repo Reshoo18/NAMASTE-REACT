f06436026f900c55b537eb27d863f4d254c12ddf
Rajnish Ranjan <ranjanrajnish64@gmail.com>
2025-04-04 14:25:18 +0530

    react code 1

--- INCEPTION/1React.js
+++ INCEPTION/1React.js
@@ -1,12 +1,16 @@
 const parent =React.createElement(
     "div",
-    {id:"parent"},
-    React.createElement(
-        "div",
-        {id:"child"},
-        React.createElement("h1",{},"i m a H1 tag")
-    )
-);
+    {id:"parent"},[
+    React.createElement("div",{id:"child"},[
+        React.createElement("h1",{},"i m a H1 tag"),
+        React.createElement("h2",{},"hi i ma h2 tag"),
+        ]),
+    React.createElement("div",{id:"child2"},[
+        React.createElement("h1",{},"i m a H1 tag"),
+        React.createElement("h2",{},"hi i ma h2 tag"),
+        ]),
+    
+    ]);
 
 
      console.log(parent);//object
