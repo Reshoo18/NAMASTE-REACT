f02076f233562bc191288e457dd53030cef8bf99
Rajnish Ranjan <ranjanrajnish64@gmail.com>
2025-04-10 13:52:08 +0530

    new add in ep 2

--- 1React.js
+++ 1React.js
@@ -1,8 +1,10 @@
+import React from "react";
+import ReactDom from "react-dom";
 const parent =React.createElement(
     "div",
     {id:"parent"},[
     React.createElement("div",{id:"child"},[
-        React.createElement("h1",{},"i m a H1 tag"),
+        React.createElement("h1",{},"this is namste react"),
         React.createElement("h2",{},"hi i ma h2 tag"),
         ]),
     React.createElement("div",{id:"child2"},[
@@ -16,4 +18,4 @@ const parent =React.createElement(
      console.log(parent);//object
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+root.render(parent); 
